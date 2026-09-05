bde54193dd1686453e5bbe10d151648d152dc46f
iamanmol45 <anmol.jana54@nmims.in>
2026-09-05 19:41:22 +0530

    Allow production frontend CORS origin

--- api/main.py
+++ api/main.py
@@ -33,6 +33,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://revenue-recovery-engine-jade.vercel.app",
 ]
 
 app.add_middleware(
